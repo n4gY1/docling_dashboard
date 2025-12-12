a50bb94fcbbef795b7cb7da33b431b61a4e637a6
Nagy Istvan <68388371+n4gY1@users.noreply.github.com>
2025-12-12 14:22:13 +0100

    Update settings.py
    d

--- docling_dashboard/settings.py
+++ docling_dashboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7-bdjgk_kb18%ivqphk8+pqrqdf%hap6xz1imvtl8o8)_3@6yu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
